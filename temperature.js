8252aa796dc7c23b6d3ddff6455b9fe73492f2e4
alu0100845235 <alu0100845235@ull.edu.es>
2016-02-24 22:44:44 +0000

    Actualizado temperature.js

--- temperature.js
+++ temperature.js
@@ -2,25 +2,31 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /^([-+]?\d+(?:\.\d*)?)\s*(?:e([-+]?\d+))?\s*(f$|fa$|fah$|fahrenheit$|c$|cel$|celsius$)/i;
   var m = temp.match(regexp);
   
   if (m) {
     var num = m[1];
-    var type = m[2];
+    var exp = m[2];
+    var type = m[3].charAt(0);
     num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
+    
+    // Calculamos exponente si lo hay
+    if (exp) {
+      exp = parseInt(exp);
+      num = num * Math.pow(10, exp);
     }
-    else {
+    
+    // Convertimos
+    if (type == 'c' || type == 'C') {
+        result = (num * 9/5)+32;
+        result = result.toFixed(1)+" Fahrenheit"
+    } else {
       result = (num - 32)*5/9;
       result = result.toFixed(1)+" Celsius"
     }
     alert(result);
-  }
-  else {
-    alert("ERROR! Try something like '-4.2C' instead");
+  } else {
+    alert("ERROR! Try something like '-4.2C or 42e-1 C' instead");
   }
 }
